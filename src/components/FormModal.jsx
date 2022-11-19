2349ed300e7ec83d1643591dc85b9e292bbdb234
Hubert Podgórski <hubert.podgorski95@gmail.com>
2022-11-19 17:57:07 +0100

    Change event form sizing

--- src/components/FormModal.jsx
+++ src/components/FormModal.jsx
@@ -15,15 +15,36 @@ const DialogContentStyled = styled(DialogContent)(({ theme }) => ({
   minWidth: 400,
   [theme.breakpoints.down("md")]: {
     minWidth: 300,
+    padding: theme.spacing(1),
   },
 }));
 
-const FormModal = ({ children, open, onClose, title }) => (
-  <Dialog open={open} onClose={onClose}>
-    <DialogTitle>{title}</DialogTitle>
+const FormModal = ({ children, open, onClose, title }) => {
+  const theme = useTheme();
 
-    <DialogContentStyled>{children}</DialogContentStyled>
-  </Dialog>
-);
+  return (
+    <Dialog
+      open={open}
+      onClose={onClose}
+      sx={{
+        [theme.breakpoints.down("md")]: {
+          margin: theme.spacing(1),
+        },
+      }}
+    >
+      <DialogTitle
+        sx={{
+          [theme.breakpoints.down("md")]: {
+            padding: theme.spacing(1),
+          },
+        }}
+      >
+        {title}
+      </DialogTitle>
+
+      <DialogContentStyled>{children}</DialogContentStyled>
+    </Dialog>
+  );
+};
 
 export default FormModal;
--- src/pages/userPanel/UserPanel.jsx
+++ src/pages/userPanel/UserPanel.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import UserBottomNavBar from "./UserBottomNavBar";
+import { Box, useTheme } from "@mui/material";
 
 const UserPanel = () => {
+  const theme = useTheme();
+
   return (
-    <>
+    <Box sx={{ marginBottom: theme.spacing(7) }}>
       <Outlet />
 
       <UserBottomNavBar />
-    </>
+    </Box>
   );
 };
 
