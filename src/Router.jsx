ab6968ec6739a7ed5096a13d7d81305371e56e50
Hubert Podgórski <hubert.podgorski95@gmail.com>
2022-11-26 01:16:39 +0100

    try different router

--- src/Router.jsx
+++ src/Router.jsx
@@ -2,11 +2,11 @@ import React, { useEffect } from "react";
 import { Navigate, Route, Routes, useLocation } from "react-router-dom";
 import {
   adminPaths,
-  adminRoutes,
+  notAuthenticatedRoutes,
   userPaths,
-  userRoutes,
 } from "./helpers/routesAndPaths";
 import UserPanel from "./pages/userPanel/UserPanel";
+import AdminPanel from "./pages/adminPanel/AdminPanel";
 import Tasks from "./pages/userPanel/Tasks";
 import AdminTasks from "./pages/adminPanel/Tasks";
 import Calendar from "./pages/userPanel/Calendar";
@@ -18,8 +18,6 @@ import SignupForm from "./pages/forms/SignupForm";
 import { useAuthContext } from "./hooks/useAuthContext";
 import { useIsAdmin } from "./hooks/useIsAdmin";
 import DogTasks from "./pages/adminPanel/DogTasks";
-import EventTemplates from "./pages/adminPanel/EventTemplates";
-import AdminPanel from "./pages/adminPanel/AdminPanel";
 
 const Router = () => {
   const { user } = useAuthContext();
@@ -36,35 +34,35 @@ const Router = () => {
       {!!user && (
         <>
           <Route path={userPaths.root} element={<UserPanel />}>
+            <Route index element={<Navigate to={userPaths.tasks} />} />
+
             <Route path={userPaths.tasks} element={<Tasks />} />
             <Route path={userPaths.calendar} element={<Calendar />} />
           </Route>
 
           {isAdmin && (
             <Route path={adminPaths.root} element={<AdminPanel />}>
-              <Route
-                index
-                element={<Navigate to={adminRoutes.tasks} replace />}
-              />
+              <Route index element={<Navigate to={adminPaths.tasks} />} />
 
               <Route path={adminPaths.tasks} element={<AdminTasks />} />
               <Route path={adminPaths.users} element={<Users />} />
               <Route path={adminPaths.dogTasks} element={<DogTasks />} />
               <Route path={adminPaths.dogs} element={<Dogs />} />
               <Route path={adminPaths.events} element={<Events />} />
-              <Route
-                path={adminPaths.eventTemplates}
-                element={<EventTemplates />}
-              />
             </Route>
           )}
         </>
       )}
 
+      <Route index element={<UserPanel />} />
+
       <Route path={"/login"} element={<LoginForm />} />
       <Route path={"/signup"} element={<SignupForm />} />
 
-      <Route path="*" element={<Navigate to={userRoutes.tasks} />} />
+      <Route
+        path="*"
+        element={<Navigate to={notAuthenticatedRoutes.login} />}
+      />
     </Routes>
   );
 };
